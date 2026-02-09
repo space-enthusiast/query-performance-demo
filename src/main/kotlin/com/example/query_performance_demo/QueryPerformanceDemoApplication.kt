7e81eba5dd33d3c5ab8dc283de58d6a7a0b3a12e
space-enthusiast <jinding0829@gmail.com>
2026-02-09 10:24:35 +0900

    init

---
+++ src/main/kotlin/com/example/query_performance_demo/QueryPerformanceDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.query_performance_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QueryPerformanceDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<QueryPerformanceDemoApplication>(*args)
+}
